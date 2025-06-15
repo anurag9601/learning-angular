b43432d8f04fe5c29ff43370517c9369cba574df
anurag9601 <anuragmishrap13@gmail.com>
2025-06-15 23:47:34 +0530

    creating todo using angular

--- todo-app/src/app/app.ts
+++ todo-app/src/app/app.ts
@@ -1,12 +1,33 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+interface todoDataType {
+  id: number,
+  todoName: string,
+  completed: boolean
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, NgFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected title = 'todo-app';
+  inputTodo: string = "";
+  allTodos: todoDataType[] = [];
+
+  handleSetNewTodo() {
+    if (this.inputTodo.trim() !== "") {
+      const newTodo: todoDataType = {
+        id: Date.now(),
+        todoName: this.inputTodo,
+        completed: false
+      };
+      this.allTodos.push(newTodo);
+      this.inputTodo = "";
+    }
+  }
 }

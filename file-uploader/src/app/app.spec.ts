41ae2dc2002fce87a33e51708eaac80b3d92e648
anurag9601 <anuragmishrap13@gmail.com>
2025-06-17 23:33:04 +0530

    create upload project

--- file-uploader/src/app/app.spec.ts
+++ file-uploader/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, routing');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, file-uploader');
   });
 });
--- file-uploader/src/app/app.ts
+++ file-uploader/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'routing';
+  protected title = 'file-uploader';
 }
